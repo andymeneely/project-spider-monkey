c0dc5133278fae7d1d083c1fda6a5f6a917c415a
Andy Meneely <andy@se.rit.edu>
2015-06-19 23:59:58 -0400

    Getting initial layout working

--- deck.rb
+++ deck.rb
@@ -1,6 +1,17 @@
+require 'pp'
 require 'squib'
+require_relative 'squib_helpers'
 
-Squib::Deck.new(cards: 3, layout: 'layout.yml') do
-  text str: 'Hello, World!'
+deck = Squib.xlsx file: 'data/deck.xlsx'
+pp deck
+deck = explode_quantities(deck)
+
+Squib::Deck.new(cards: deck['Name'].size, layout: 'layout.yml',) do
+  
+  %w(Name).each do |key|
+    text str: deck[key], layout: key.downcase
+  end
+
+  png file: 'tgc-proof-overlay.png'
   save format: :png
 end
